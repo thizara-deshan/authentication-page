8e90f4b913674063e76eea3748158ebec9c5b365
Thizara <thizaradeshan@gmail.com>
2024-10-08 08:04:33 +0530

    removed unwanted modules

--- auth.config.ts
+++
@@ -1,7 +0,0 @@
-import GitHub from "next-auth/providers/github";
-import type { NextAuthConfig } from "next-auth";
-
-// Notice this is only an object, not a full Auth.js instance
-export default {
-  providers: [GitHub],
-} satisfies NextAuthConfig;
